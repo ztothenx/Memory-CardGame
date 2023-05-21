251e03e8a6e841a0f13292eeed185b13ceb77f7c
ztothenx <127930052+ztothenx@users.noreply.github.com>
2023-05-21 20:21:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.delete('/create', jsonParser, function (req, res, next) {
                 if (err) {
                     console.log(err);
                 } else {
-                    res.send("Delete data already");
+                    res.send(result);
                 }
             });
     });
