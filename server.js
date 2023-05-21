9cc9ae82a0df1fd5fde10962dc28d187a0bf3bfc
ztothenx <127930052+ztothenx@users.noreply.github.com>
2023-05-21 18:55:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/create', jsonParser, function (req, res, next) {
     });
 
 
-app.get('/create', jsonParser, function (req, res, next) {
+app.get('/gameusers', jsonParser, function (req, res, next) {
         connection.execute(
             'SELECT * FROM gameuser', (err, result) => {
                 if (err) {
